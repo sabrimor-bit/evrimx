b8e1f772db123b3e84c5602afdb87514af52257d
Sabri Mor <sabri.mor@evrimbilgisayar.com>
2026-03-26 10:42:26 +0300

    tasarım guncellendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="tr">
+    <html lang="tr" className="dark">
       <body className="bg-gray-50 min-h-screen">{children}</body>
     </html>
   );
